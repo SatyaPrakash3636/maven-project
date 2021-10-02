c293530abe9e98bef32b512a0bee277e8fb9aedd
jenkins <jenkins@localhost.localdomain>
2021-10-02 20:55:43 +0530

    Fixed Path in mvn build and test script

--- jenkins/build/mvn.sh
+++ jenkins/build/mvn.sh
@@ -3,5 +3,7 @@
 echo "**************************************"
 echo "***** Building Application Jar *******"
 echo "**************************************"
+
+WORKSPACE=/home/jenkins/jenkins-data/jenkins_home/workspace/pipeline/maven-remote
  
-docker run --rm -v /home/jenkins/jenkins-data/pipeline/java-app:/app -v /root/.m2:/root/.m2 -w /app maven:3.8.2-jdk-11 "$@"
+docker run --rm -v $WORKSPACE/java-app:/app -v /root/.m2:/root/.m2 -w /app maven:3.8.2-jdk-11 "$@"
