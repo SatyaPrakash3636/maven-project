8660f24f7c78a1d1db6f2fe8471001468cb03280
jenkins <jenkins@localhost.localdomain>
2021-10-03 19:45:01 +0530

    updated deploy script

--- jenkins/deploy/remote-deploy.sh
+++ jenkins/deploy/remote-deploy.sh
@@ -1,10 +1,10 @@
 #! /bin/bash
 
 export IMAGE=$(sed -n '1p' /tmp/.auth)
-export TAG=$(sed -n '2p' /tmp/.auth)
+export BUILD_TAG=$(sed -n '2p' /tmp/.auth)
 export DOC_USR=$(sed -n '3p' /tmp/.auth)
 export DOC_PASS=$(sed -n '4p' /tmp/.auth)
-NEXUS_URL="localhost:8123"
+export NEXUS_URL="localhost:8123"
 docker login -u $DOC_USR -p $DOC_PASS 192.168.1.102:8123
 docker pull $NEXUS_URL/$IMAGE:$BUILD_TAG
 docker-compose up -d
