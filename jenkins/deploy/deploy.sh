0af2bd75ffe244f334d6c05846d0276597ce39c4
jenkins <jenkins@localhost.localdomain>
2021-10-03 18:27:11 +0530

    some update

--- jenkins/deploy/deploy.sh
+++ jenkins/deploy/deploy.sh
@@ -10,7 +10,7 @@ echo $BUILD_TAG >> ./.auth
 echo $DOCKER_USER >> ./.auth
 echo $DOCKER_PASS >> ./.auth
 chmod 400 jenkins/deploy/prodcert
-scp -i jenkins/deploy/prodcert .auth remote_user@prod_host:/tmp/.auth
-scp -i jenkins/deploy/prodcert remote-deploy.sh remote_user@prod_host:/tmp/remote-deploy.sh
-ssh -i jenkins/deploy/prodcert remote_user@prod_host /tmp/remote-deploy.sh
+/usr/bin/scp -i jenkins/deploy/prodcert .auth remote_user@prod_host:/tmp/.auth
+/usr/bin/scp -i jenkins/deploy/prodcert remote-deploy.sh remote_user@prod_host:/tmp/remote-deploy.sh
+/usr/bin/ssh -i jenkins/deploy/prodcert remote_user@prod_host /tmp/remote-deploy.sh
 
