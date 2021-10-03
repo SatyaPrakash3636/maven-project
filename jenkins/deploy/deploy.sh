6c4f62fe99d7b42e043ba713092f61f9937ba0d7
jenkins <jenkins@localhost.localdomain>
2021-10-03 18:17:17 +0530

    minor update

--- jenkins/deploy/deploy.sh
+++ jenkins/deploy/deploy.sh
@@ -9,7 +9,7 @@ echo mavan-project > ./.auth
 echo $BUILD_TAG >> ./.auth
 echo $DOCKER_USER >> ./.auth
 echo $DOCKER_PASS >> ./.auth
-
+chmod 400 jenkins/deploy/prodcert
 scp -i jenkins/deploy/prodcert .auth remote_user@prod_host:/tmp/.auth
 scp -i jenkins/deploy/prodcert remote-deploy.sh remote_user@prod_host:/tmp/remote-deploy.sh
 ssh -i jenkins/deploy/prodcert remote_user@prod_host /tmp/remote-deploy.sh
