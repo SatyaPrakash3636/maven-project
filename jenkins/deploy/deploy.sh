e3550ec7d8912ff0e0a230e7b64e667315b62e89
jenkins <jenkins@localhost.localdomain>
2021-10-03 19:15:59 +0530

    minor update

--- jenkins/deploy/deploy.sh
+++ jenkins/deploy/deploy.sh
@@ -5,12 +5,12 @@ echo "****** Deploying Docker Image to Remote ********"
 echo "************************************************"
 
 DOCKER_USER=admin
-echo mavan-project > ./.auth
-echo $BUILD_TAG >> ./.auth
-echo $DOCKER_USER >> ./.auth
-echo $DOCKER_PASS >> ./.auth
+echo mavan-project > jenkins/deploy/.auth
+echo $BUILD_TAG >> jenkins/deploy/.auth
+echo $DOCKER_USER >> jenkins/deploy/.auth
+echo $DOCKER_PASS >> jenkins/deploy/.auth
 chmod 400 jenkins/deploy/prodcert
-/usr/bin/scp -i jenkins/deploy/prodcert .auth remote_user@prod_host:/tmp/.auth
-/usr/bin/scp -i jenkins/deploy/prodcert remote-deploy.sh remote_user@prod_host:/tmp/remote-deploy.sh
+/usr/bin/scp -i jenkins/deploy/prodcert jenkins/deploy/.auth remote_user@prod_host:/tmp/.auth
+/usr/bin/scp -i jenkins/deploy/prodcert jenkins/deploy/remote-deploy.sh remote_user@prod_host:/tmp/remote-deploy.sh
 /usr/bin/ssh -i jenkins/deploy/prodcert remote_user@prod_host /tmp/remote-deploy.sh
 
